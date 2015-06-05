aaa208d3345c697b585c7be1d59c20b738b666df
maricosi <marisaisimoes@gmail.com>
2015-06-05 16:16:09 +0100

    16:15 05/06 Listagem de musica por utilizador logado

--- proj4ejb/src/main/java/pt/uc/dei/aor/paj/fachada/IntMusicFachada.java
+++ proj4ejb/src/main/java/pt/uc/dei/aor/paj/fachada/IntMusicFachada.java
@@ -13,6 +13,7 @@ public interface IntMusicFachada {
 	public abstract void delete(Music music);
 	public abstract Music find(int entityID);
 	public abstract List<Music> findAll();
+	public abstract List<Music> findAllByUtilizador(String username, String password);
 	public abstract List<Music> findAllByTitleArtist(String title, String artist);
 	public abstract List<Music> findAllByTitle(String title);
 	public abstract List<Music> findAllByArtist(String artist);
